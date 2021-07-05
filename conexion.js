dc69d557ee6d03f7c5f310f3b67084b98a730d5c
Julio Quinchiguango <jcquinchiguango@utn.edu.ec>
2021-07-04 23:36:26 -0500

    ultimo commit

--- conexion.js
+++ conexion.js
@@ -1,10 +1,11 @@
 const pgPromise = require("pg-promise")
 const config ={
-    host:"localhost",
+    host:"ec2-34-232-191-133.compute-1.amazonaws.com",
     port:"5432",
-    database:"turismo",
-    user:"postgres",
-    password:"1234"
+    database:"d9ov9ci4le66rm",
+    user:"uozyxyntleytaz",
+    password:"a4cb1f54a864be4a0e40427ec9b3c379831011d41e9fc80689896cbf7858296f",
+    ssl:true 
 }
 
 const pgp = pgPromise({})
--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const express = require("express")
 const app = express()
 const bodyParser =require("body-parser")
+const cors=require('cors')
 
 //middleware ayuda intermediario para configurr las insatcias del servidor
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+//cors
+app.use(cors())
 
 //routes voy a tener varias rutas para interactuar con mi api
 app.use(require("./routers/index"))
 
 //Execution 
 app.get('/',(req,res)=> [res.send('Welcome to Puntos de antención de Turismo API ')])
-app.listen(3001)
-console.log("Server running in http://localhost:3001")
+/* app.listen(3001)
+console.log("Server running in http://localhost:3001") */
+//ejecucion
+//app.listen(3000)
+app.listen(process.env.PORT || 5050)
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+console.log("Server running in http://localhost:5050")
